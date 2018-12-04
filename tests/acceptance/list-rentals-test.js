41e85f732820b01bd5f45a8c83d8a5330d2a1a83
fridl8 <tdf970@gmail.com>
2018-12-04 11:10:40 -0600

    Add click and url check for nav bar, both passing

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -9,4 +9,16 @@ module('Acceptance | list rentals', function(hooks) {
     await visit('/');
     assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
+
+  test('should link to information about the company', async function(assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+
+  test('should link to contact information', async function(assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
 });
