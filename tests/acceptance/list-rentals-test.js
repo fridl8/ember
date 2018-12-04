e09f3f1af6bc717aeb8fc96ef52728307186f79e
fridl8 <tdf970@gmail.com>
2018-12-04 16:35:17 -0600

    Add test for filtering list by city

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -28,4 +28,12 @@ module('Acceptance | list rentals', function(hooks) {
     await visit('/');
     assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
+
+  test('should filter the list of rentals by city', async function(assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(this.element.querySelectorAll('.results .listing').length, 1, 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
+  });
 });
