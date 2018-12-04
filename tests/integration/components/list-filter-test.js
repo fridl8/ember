a4d5c66caffbebc8d7029d596afeafa9379cc474
fridl8 <tdf970@gmail.com>
2018-12-04 16:03:38 -0600

    Add initial blank test for checking listings

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -6,21 +6,7 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | list-filter', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('should initially load all listings', async function(assert) {
 
-    await render(hbs`{{list-filter}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#list-filter}}
-        template block text
-      {{/list-filter}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
