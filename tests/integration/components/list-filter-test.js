aaeae20bf1f54afec721b083282dd63dce138279
fridl8 <tdf970@gmail.com>
2018-12-04 16:15:24 -0600

    Add initial filter lines for test

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -2,11 +2,32 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { resolve } from 'rsvp';
+
+const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
+const FILTERED_ITEMS = [{city: 'San Francisco'}];
 
 module('Integration | Component | list-filter', function(hooks) {
   setupRenderingTest(hooks);
 
   test('should initially load all listings', async function(assert) {
+    // we want our actions to return promises,
+    // since they are potentially fetching data asynchronously
+    this.set('filterByCity', () => resolve({ results: ITEMS }));
 
+    // with an integration test,
+    // you can set up and use your component in the same way your application
+    // will use it.
+    await render(hbs`
+        {{#list-filter filter=(action filterByCity) as |results|}}
+          <ul>
+          {{#each results as |item|}}
+            <li class="city">
+              {{item.city}}
+            </li>
+          {{/each}}
+          </ul>
+        {{/list-filter}}
+      `);
   });
 });
